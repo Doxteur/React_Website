c1096a3b7ecfc275b55e8f6a188cc8aea439fd3d
Erik Martin <erik@linaia.com>
2022-09-23 15:00:46 +0200

    First commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
+import Survey from './pages/Survey';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Routes, Route } from 'react-router'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/survey" >
+          <Survey />
+        </Route>
+      </Routes>
+    </Router>
+
+  </React.StrictMode >
 );
 
 // If you want to start measuring performance in your app, pass a function
